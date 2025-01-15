36f676871c96c97ed44924431ab8ac97299ffb0f
SStakhovski <sstahovsky@insoftgroup.com>
2025-01-15 14:59:07 +0100

    checkout action

--- frontend/src/middleware/cart.ts
+++ frontend/src/middleware/cart.ts
@@ -1,10 +1,15 @@
 import { cartCheckout } from '@/forms/cart';
-import { handleMiddlewareRequest } from '@/utils/server';
+import { handleMiddlewareRequest, SwellServerContext } from '@/utils/server';
 
 export const submitCheckout = handleMiddlewareRequest(
   'POST',
   '/cart',
-  cartCheckout,
+  async (context: SwellServerContext) => {
+    cartCheckout({
+      ...context,
+      formRedirect: context.redirect,
+    });
+  },
 );
 
 export default [submitCheckout];
