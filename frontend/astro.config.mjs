fb2312f747b1b1f933a8e594c1257ea0db6c9ff1
dminch <dminch@gmail.com>
2025-09-23 22:59:51 +0300

    cors

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,6 +6,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  security: {
+    checkOrigin: false
+  },
   adapter: cloudflare({
     imageService: 'cloudflare',
     platformProxy: {
